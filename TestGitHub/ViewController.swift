7d2fdc7c63cde02ac7381c6bcecf4ceb5bf458bf
tannt275 <tannt275@gmail.com>
2016-08-03 10:38:06 +0700

    second commit

---
+++ TestGitHub/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestGitHub
+//
+//  Created by Tan Nguyen on 8/3/16.
+//  Copyright © 2016 Tan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
